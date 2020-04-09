4d5795db49d341e6149aea089560552de4b26551
David Faber <david@1bios.co>
2020-04-09 14:56:36 -0700

    Add explicit callback URL override to fix underlying omniauth-oauth2 bug
    See https://github.com/jackdempsey/omniauth-reddit/pull/12 and
    https://github.com/omniauth/omniauth-oauth2/issues/81

--- lib/omniauth/strategies/withings.rb
+++ lib/omniauth/strategies/withings.rb
@@ -19,6 +19,10 @@ module OmniAuth
       uid do
         access_token.params['userid']
       end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
